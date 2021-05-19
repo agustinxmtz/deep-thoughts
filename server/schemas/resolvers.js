7b4ae508c17796693889b79bc6abb617a5baefbd
Agustin <agustinxmtz@gmail.com>
2021-05-19 13:26:17 -0500

    Authorize Users with JWTs'

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -31,8 +32,9 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
 
-            return user;
+            return { token, user };
 
         },
         login: async (parent, { email, password }) => {
@@ -48,7 +50,9 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
-            return user;
+
+            const token = signToken(user);
+            return { token, user };
 
         }
     }
