324ad932160b571ff7ba1daa9743d1236fd95308
Agustin <agustinxmtz@gmail.com>
2021-05-19 22:15:51 -0500

    Add Mutations for Thoughts, Friends, and Reactions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
         thoughtText : String
         createdAt: String
         username: String
-        rectionCount: Int
+        reactionCount: Int
         reactions: [Reaction]
     }
 
@@ -30,16 +30,20 @@ const typeDefs = gql`
     }
 
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
-    }
+      }
 
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-    },
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+      },
 
     type Auth {
         token: ID!
